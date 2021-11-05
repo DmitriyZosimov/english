82e191f3a89488d9c5110f45815212cb2e198ec1
DmitriyZosimov <Dmitriy_Zosimov@epam.com>
2021-11-05 14:07:36 +0300

    setup rds tests: added @EnabledIf

--- aws-rds/src/test/java/com/myenglish/aws/RDSConfigTest.java
+++ aws-rds/src/test/java/com/myenglish/aws/RDSConfigTest.java
@@ -4,20 +4,24 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.EnabledIf;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import javax.sql.DataSource;
 
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(classes = RDSConfig.class)
+@PropertySource("classpath:com/myenglish/aws/aws-config.properties")
 @ActiveProfiles("aws")
 public class RDSConfigTest {
 
     @Autowired
     DataSource dataSource;
 
+    @EnabledIf(expression = "${tests.enabled}", loadContext = true)
     @Test
     public void setupDataSourceTest() {
         Assertions.assertNotNull(dataSource);
