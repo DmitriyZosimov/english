d177a3a5a52ee0b26b007e6e27485ffacb4ce3bf
DmitriyZosimov <Dmitriy_Zosimov@epam.com>
2022-03-04 17:43:31 +0300

    added logger to web-angular

--- web-angular/src/main/java/com/myenglish/web/angular/config/WebAngularConfig.java
+++ web-angular/src/main/java/com/myenglish/web/angular/config/WebAngularConfig.java
@@ -2,6 +2,9 @@ package com.myenglish.web.angular.config;
 
 import com.myenglish.aws.RDSConfig;
 import com.myenglish.dao.config.DaoHibernateConfig;
+import com.myenglish.kafka.logger.LoggerConsumerConfig;
+import com.myenglish.kafka.logger.LoggerProducerConfig;
+import com.myenglish.kafka.logger.LoggerProducerWithoutKafkaConfig;
 import com.myenglish.service.config.ServiceConfig;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -11,7 +14,8 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @ComponentScan(basePackages = {"com.myenglish.web.angular"})
-@Import({ServiceConfig.class, DaoHibernateConfig.class, RDSConfig.class})
+@Import({ServiceConfig.class, DaoHibernateConfig.class, RDSConfig.class,
+        LoggerProducerConfig.class, LoggerConsumerConfig.class, LoggerProducerWithoutKafkaConfig.class})
 @Profile("aws")
 @EnableWebMvc
 public class WebAngularConfig {
--- web-angular/src/main/java/com/myenglish/web/angular/config/WebAngularLocalConfig.java
+++ web-angular/src/main/java/com/myenglish/web/angular/config/WebAngularLocalConfig.java
@@ -1,6 +1,9 @@
 package com.myenglish.web.angular.config;
 
 import com.myenglish.dao.config.DaoHibernateConfig;
+import com.myenglish.kafka.logger.LoggerConsumerConfig;
+import com.myenglish.kafka.logger.LoggerProducerConfig;
+import com.myenglish.kafka.logger.LoggerProducerWithoutKafkaConfig;
 import com.myenglish.localdb.PostgreSQLDBConfig;
 import com.myenglish.service.config.ServiceConfig;
 import org.springframework.context.annotation.ComponentScan;
@@ -11,7 +14,8 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @ComponentScan(basePackages = {"com.myenglish.web.angular"})
-@Import({ServiceConfig.class, DaoHibernateConfig.class, PostgreSQLDBConfig.class})
+@Import({ServiceConfig.class, DaoHibernateConfig.class, PostgreSQLDBConfig.class,
+        LoggerProducerConfig.class, LoggerConsumerConfig.class, LoggerProducerWithoutKafkaConfig.class})
 @Profile("local")
 @EnableWebMvc
 public class WebAngularLocalConfig {
