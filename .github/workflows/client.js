e90f83adbc24e772672f850c62e495c05a7baae9
DmitriyZosimov <Dmitriy_Zosimov@epam.com>
2022-05-05 12:49:12 +0300

    added a postgresql image to github actions

---
+++ .github/workflows/client.js
@@ -0,0 +1,48 @@
+const { Client } = require('pg');
+
+const pgclient = new Client({
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: 'postgres',
+    password: 'postgres',
+    database: 'wordsdb'
+});
+
+pgclient.connect();
+
+const table = 'CREATE TABLE words (\n' +
+    '    id INTEGER NOT NULL AUTO_INCREMENT,\n' +
+    '    english varchar(50) NOT NULL,\n' +
+    '    russian varchar(50) NOT NULL,\n' +
+    '    description varchar(255),\n' +
+    '    date_of_registry date,\n' +
+    '    transcription varchar(50),\n' +
+    '    PRIMARY KEY(id)\n' +
+    ');\n' +
+    '\n' +
+    'CREATE TABLE verbs (\n' +
+    '    id INTEGER NOT NULL AUTO_INCREMENT,\n' +
+    '    russian varchar(50) NOT NULL,\n' +
+    '    first_form varchar(50) NOT NULL,\n' +
+    '    second_form varchar(50) NOT NULL,\n' +
+    '    third_form varchar(50) NOT NULL,\n' +
+    '    description varchar(255),\n' +
+    '    date_of_registry date,\n' +
+    '    PRIMARY KEY(id)\n' +
+    ');'
+const text = 'INSERT INTO words(english, russian, description, date_of_registry, transcription) VALUES($1, $2, $3, $4, $5) RETURNING *'
+const values = ['hello', 'здравствуйте', 'здравствуйте', '2021-01-01', null]
+
+pgclient.query(table, (err, res) => {
+    if (err) throw err
+});
+
+pgclient.query(text, values, (err, res) => {
+    if (err) throw err
+});
+
+pgclient.query('SELECT * FROM student', (err, res) => {
+    if (err) throw err
+    console.log(err, res.rows) // Print the data in student table
+    pgclient.end()
+});
